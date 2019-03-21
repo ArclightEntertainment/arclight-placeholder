c21d32b17196da45033de4c41de54731356fa264
Shalin Lathigra <32829047+ShalinLathigra@users.noreply.github.com>
2019-03-20 20:01:10 -0400

    Add files via upload
    Added Addresses for Clients 5-20

---
+++ data/populate/addresses/17.sql
@@ -0,0 +1,18 @@
+
+INSERT OR REPLACE INTO Address(
+		clientID,
+		streetLine1,
+		streetLine2,
+		city,
+		subnationalDivision,
+		country,
+		postalCode
+	)VALUES(
+		17,
+		"231 Emma Street",
+		"",
+		"Gaspésie-Sud",
+		"Quebec",
+		"Canada",
+		"G0C0G0"
+);
