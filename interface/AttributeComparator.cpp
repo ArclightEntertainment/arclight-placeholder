12e3e1421970a2efb69276877a9df703bb028d80
Brian_grickites <briangrickites@cmail.carleton.ca>
2019-04-07 23:09:58 -0400

    Attribute comparator working

--- interface/AttributeComparator.cpp
+++ interface/AttributeComparator.cpp
@@ -5,12 +5,13 @@
 
 float AttributeComparator::calculateCompatibility(UAnimal *animal, UClient *client)
 {
-    float random = 100.0f*(static_cast <float> (rand()) / static_cast <float>(RAND_MAX));
-    return random;
-    /*
+    //float random = 100.0f*(static_cast <float> (rand()) / static_cast <float>(RAND_MAX));
+    //return random;
+
     //Temporary
     float totalRating = 0.0f;
-    float overCost = (animal->species.eCostPerMonth - client->profile.budgetPerMonth)/client->profile.budgetPerMonth;
+    float overCost = (animal->getInt(22) - client->getInt(15))/client->getInt(15);
+
     if(overCost < 0){
         overCost = 0.0f;
     }
@@ -19,122 +20,131 @@ float AttributeComparator::calculateCompatibility(UAnimal *animal, UClient *clie
         return -1;
     }
 
-    if(animal->affinityForChildren == 0 && client->hasChildrenUnder12)
+    if(animal->getInt(14) == 0 && client->getBool(2))
     {
         return -1;
     }
 
+    if(client->getInt(14) - (animal->getInt(5) - animal->getInt(3)) > 4 || client->getInt(14) - (animal->getInt(5) - animal->getInt(3)) < -4.0f)
+        return -1;
 
-    if((int)client->profile.levelOfPatience - (4 - (int)animal->affinityForPeople*2) < 0)
+    if(client->getInt(18) - (4 - animal->getInt(12)*2) < 0)
     {
-        totalRating += ((4 - (int)animal->affinityForPeople*2) - (int)client->profile.levelOfPatience) * 0.04f;
-    }
-
-    if((int)client->profile.physicalAffection - (4 - (int)animal->affinityForPeople)*2 < 0)
-    {
-        totalRating += ((4 - (int)animal->affinityForPeople*2) - (int)client->profile.physicalAffection) * 0.06f;
-    }
-
-    if((int)client->profile.previousExperience - (int)animal->levelOfCare*2 < 0)
-    {
-        totalRating += (((int)animal->levelOfCare*2) - (int)client->profile.previousExperience) * 0.04f;
-    }
-
-    if((int)client->profile.previousExperience - (4 - (int)animal->trainingLevel*2) < 0)
-    {
-        totalRating += ((4 - (int)animal->trainingLevel*2) - (int)client->profile.previousExperience) * 0.06f;
-    }
-
-    if((int)client->profile.levelOfPatience - (4 - (int)animal->trainabilityLevel*2) < 0)
-    {
-        totalRating += ((4 - (int)animal->trainabilityLevel*2) - (int)client->profile.levelOfPatience) * 0.04f;
-    }
-
-    if((int)client->profile.previousExperience - (4 - (int)animal->trainabilityLevel*2) < 0)
-    {
-        totalRating += ((4 - (int)animal->trainabilityLevel*2) - (int)client->profile.previousExperience) * 0.04f;
-    }
-
-    if((int)client->profile.levelOfPatience - (4 - (int)animal->approachability*2) < 0)
-    {
-        totalRating += ((4 - (int)animal->approachability*2) - (int)client->profile.levelOfPatience) * 0.06f;
-    }
-
-    if((int)client->profile.previousExperience - (4 - (int)animal->approachability*2) < 0)
-    {
-        totalRating += ((4 - (int)animal->approachability*2) - (int)client->profile.previousExperience) * 0.02f;
-    }
-
-    if((int)client->profile.levelOfMobility - (int)animal->levelOfCare*2 < 0)
-    {
-        totalRating += (((int)animal->levelOfCare*2) - (int)client->profile.levelOfMobility) * 0.06f;
-    }
-
-    if((int)client->profile.levelOfMobility - (4-(int)animal->trainingLevel*2) < 0)
-    {
-        totalRating += ((4-((int)animal->trainingLevel)*2) - (int)client->profile.levelOfMobility) * 0.02f;
-    }
-
-    if((int)client->profile.levelOfPatience - (4-(int)animal->trainingLevel*2) < 0)
-    {
-        totalRating += ((4-((int)animal->trainingLevel)*2) - (int)client->profile.levelOfPatience) * 0.06f;
-    }
-
-    if((int)client->profile.levelOfMobility - (int)animal->levelOfEnergy*2 < 0)
-    {
-        totalRating += (((int)animal->levelOfEnergy*2) - (int)client->profile.levelOfMobility) * 0.06f;
-    }
-
-    if((client->profile.timeAvailabilityPerDay / 30) - (4-(int)animal->trainingLevel*2))
-    {
-        totalRating += ((4-(int)animal->trainingLevel*2) - (client->profile.timeAvailabilityPerDay / 30)) * 0.04f;
-    }
-
-    if((int)animal->timeCommitment == 0 && 15 - client->profile.timeAvailabilityPerDay > 0)
-    {
-        totalRating += ((15.0f - client->profile.timeAvailabilityPerDay)/15.0f) * 4 * 0.06;
-    }
-    else if((int)animal->timeCommitment == 1 && 60 - client->profile.timeAvailabilityPerDay > 0)
-    {
-        totalRating += ((60.0f - client->profile.timeAvailabilityPerDay)/60.0f) * 4 * 0.06;
-    }
-    else if((int)animal->timeCommitment == 2 && 150 - client->profile.timeAvailabilityPerDay > 0)
-    {
-        totalRating += ((150.0f - client->profile.timeAvailabilityPerDay)/150.0f) * 4 * 0.06;
+        totalRating += (4 - animal->getInt(12)*2 - client->getInt(18)) * 0.04f;
     }
 
 
-    if((client->profile.timeAvailabilityPerDay / 30) - (4-(int)animal->trainabilityLevel*2))
+    if(client->getInt(20) - (4 - animal->getInt(12)*2) < 0)
     {
-        totalRating += ((4-((int)animal->trainabilityLevel)*2) - (client->profile.timeAvailabilityPerDay / 30)) * 0.04f;
+        totalRating += ((4 - animal->getInt(12)*2) - client->getInt(20)) * 0.06f;
     }
 
-    if((int)animal->levelOfCare == 0 && 15 - client->profile.timeAvailabilityPerDay > 0)
+    if(client->getInt(19) - animal->getInt(8)*2 < 0)
     {
-        totalRating += ((15.0f - client->profile.timeAvailabilityPerDay)/15.0f) * 4 * 0.06;
+        totalRating += ((animal->getInt(8)*2) - client->getInt(19)) * 0.04f;
     }
-    else if((int)animal->levelOfCare == 1 && 60 - client->profile.timeAvailabilityPerDay > 0)
+
+    if(client->getInt(19) - (4 - animal->getInt(10)*2) < 0)
     {
-        totalRating += ((60.0f - client->profile.timeAvailabilityPerDay)/60.0f) * 4 * 0.06;
+        totalRating += ((4 - animal->getInt(10)*2) - client->getInt(19)) * 0.06f;
     }
-    else if((int)animal->levelOfCare == 2 && 150 - client->profile.timeAvailabilityPerDay > 0)
+
+
+    if(client->getInt(18) - (4 - animal->getInt(11)*2) < 0)
     {
-        totalRating += ((150.0f - client->profile.timeAvailabilityPerDay)/150.0f) * 4 * 0.06;
+        totalRating += ((4 - animal->getInt(11)*2) - client->getInt(18)) * 0.04f;
+    }
+
+    if(client->getInt(19) - (4 - animal->getInt(11)*2) < 0)
+    {
+        totalRating += ((4 - animal->getInt(11)*2) - client->getInt(19)) * 0.04f;
+    }
+
+    if(client->getInt(18) - (4 - animal->getInt(15)*2) < 0)
+    {
+        totalRating += ((4 - animal->getInt(15)*2) - client->getInt(18)) * 0.06f;
+    }
+
+    if(client->getInt(19) - (4 - animal->getInt(15)*2) < 0)
+    {
+        totalRating += ((4 - animal->getInt(15)*2) - client->getInt(19)) * 0.02f;
+    }
+
+    if(client->getInt(17) - animal->getInt(8)*2 < 0)
+    {
+        totalRating += ((animal->getInt(8)*2) - client->getInt(17)) * 0.06f;
+    }
+
+    if(client->getInt(17) - (4-animal->getInt(10)*2) < 0)
+    {
+        totalRating += (4-(animal->getInt(10)*2) - client->getInt(17)) * 0.02f;
+    }
+
+    if(client->getInt(18) - (4-animal->getInt(10)*2) < 0)
+    {
+        totalRating += (4-(animal->getInt(10)*2) - client->getInt(18)) * 0.06f;
+    }
+
+    if(client->getInt(17) - animal->getInt(9)*2 < 0)
+    {
+        totalRating += ((animal->getInt(9)*2) - client->getInt(17)) * 0.06f;
+    }
+
+    if((client->getInt(16) / 30) - (4-animal->getInt(10)*2))
+    {
+        totalRating += ((4-animal->getInt(10)*2) - (client->getInt(16) / 30)) * 0.04f;
+    }
+
+    if(animal->getInt(16) == 0 && 15 - client->getInt(16) > 0)
+    {
+        totalRating += ((15.0f - client->getInt(16))/15.0f) * 4 * 0.06;
+    }
+    else if(animal->getInt(16) == 1 && 60 - client->getInt(16) > 0)
+    {
+        totalRating += ((60.0f - client->getInt(16))/60.0f) * 4 * 0.06;
+    }
+    else if(animal->getInt(16) == 2 && 150 - client->getInt(16) > 0)
+    {
+        totalRating += ((150.0f - client->getInt(16))/150.0f) * 4 * 0.06;
+    }
+
+
+    if((client->getInt(16) / 30) - (4-animal->getInt(11)*2))
+    {
+        totalRating += (4-(animal->getInt(11)*2) - (client->getInt(16) / 30)) * 0.04f;
+    }
+
+    if(animal->getInt(8) == 0 && 15 - client->getInt(16) > 0)
+    {
+        totalRating += ((15.0f - client->getInt(16))/15.0f) * 4 * 0.06;
+    }
+    else if(animal->getInt(8) == 1 && 60 - client->getInt(16) > 0)
+    {
+        totalRating += ((60.0f - client->getInt(16))/60.0f) * 4 * 0.06;
+    }
+    else if(animal->getInt(8) == 2 && 150 - client->getInt(16) > 0)
+    {
+        totalRating += ((150.0f - client->getInt(16))/150.0f) * 4 * 0.06;
     }
 
     if(overCost > 0 && overCost < 0.25f)
         totalRating += overCost * 16 * 0.06;
 
-    if(client->hasPets)
-        totalRating += (4-(int)animal->affinityForAnimals*2)*0.06;
+    if(client->getBool(3))
+        totalRating += (4-animal->getInt(14)*2)*0.06;
 
-    if(client->hasChildrenUnderTwelve)
-        totalRating += (4-(int)animal->affinityForChildren*2)*0.06;
+    if(client->getBool(2))
+        totalRating += (4-animal->getInt(13)*2)*0.06;
 
-    //ownership expectation
+    if(client->getInt(14) - (animal->getInt(5) - animal->getInt(3)) > 0)
+        totalRating += (client->getInt(14) - (animal->getInt(5) - animal->getInt(3))) * 0.06;
+
+    else if(client->getInt(14) - (animal->getInt(5) - animal->getInt(3)) < 0)
+        totalRating += -1.0f * (client->getInt(14) - (animal->getInt(5) - animal->getInt(3))) * 0.06;
 
     //float random = 100.0f*(static_cast <float> (rand()) / static_cast <float>(RAND_MAX)); //1.0;
 
-    return totalRating;
-    */
+
+    return 100.0f - ((totalRating * 100.0f)/4.0f);
+
 }
